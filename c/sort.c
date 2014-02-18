fb7266d191d38e926e57b3fc2113704cc2e8c61a
Andy Sayler <andy.sayler@gmail.com>
2014-02-18 13:42:42 -0700

    Minor bug fixes

--- c/sort.c
+++ c/sort.c
@@ -11,62 +11,71 @@
 
 #define MAX_LEN 100
 
-inline void swap(void* a, void* b, size_t size){
+inline void swap(void* a, void* b, size_t size) {
+
+    if(a == b) {
+        return;
+    }
 
     void* tmp = malloc(size);
-    if(tmp == NULL){
-	perror("swap: malloc of 'tmp' failed");
-	return;
+    if(tmp == NULL) {
+        perror("swap: malloc of 'tmp' failed");
+        return;
     }
 
     memcpy(tmp, a, size);
     memcpy(a, b, size);
     memcpy(b, tmp, size);
 
+    if(tmp) {
+        free(tmp);
+    }
+
     return;
 }
 
-inline int partition(void* array, int left, int right, int pivot, size_t size, int (*compare)(const void*, const void*)){
+inline int partition(void* array, int left, int right, int pivot, size_t size, int (*compare)(const void*, const void*)) {
 
     int i;
     void* ip = NULL;
     int store = left;
     void* storep = array + (store * size);
     void* rightp = array + (right * size);
- 
+
     swap(rightp, (array + (pivot * size)), size);
-    
-    for(i = left; i < right; i++){
-	ip = array + (i * size);
-	if(compare(ip, rightp) < 0){
-	    swap(ip, storep, size);
-	    store++;
-	    storep = array + (store * size);
-	}
+
+    for(i = left; i < right; i++) {
+        ip = array + (i * size);
+        if(compare(ip, rightp) < 0){
+            swap(ip, storep, size);
+            store++;
+            storep = array + (store * size);
+        }
     }
     swap(rightp, storep, size);
 
     return store;
+
 }
 
 void quicksort(void* array, int left, int right, size_t size, int (*compare)(const void*, const void*)){
 
     int pivot;
 
-    if(left < right){
-	pivot = ((right - left) / 2) + left;
-	pivot = partition(array, left, right, pivot, size, compare);
-	quicksort(array, left, (pivot - 1), size, compare);
-	quicksort(array, (pivot + 1), right, size, compare);
+    if(left < right) {
+        pivot = ((right - left) / 2) + left;
+        pivot = partition(array, left, right, pivot, size, compare);
+        quicksort(array, left, (pivot - 1), size, compare);
+        quicksort(array, (pivot + 1), right, size, compare);
     }
 
     return;
 }
 
 void sort(void* array, size_t len, size_t size, int (*compare)(const void*, const void*)){
-    
+
     quicksort(array, 0, len-1, size, compare);
-    
+
     return;
 }
 
@@ -86,23 +95,24 @@ int main(int argv, char* argc[]){
 
     FILE* infile = NULL;
     if(argv > 1){
-	infile = fopen(argc[1], "r");
-	if(!infile){
-	    perror("Failed to open input file:");
-	    exit(EXIT_FAILURE);
-	}
-	while((len < MAX_LEN) && (fscanf(infile, "%d", &values[len]) > 0)){
-	    len++;
-	}
+        infile = fopen(argc[1], "r");
+        if(!infile){
+            perror("Failed to open input file:");
+            exit(EXIT_FAILURE);
+        }
+        while((len < MAX_LEN) && (fscanf(infile, "%d", &values[len]) > 0)){
+            len++;
+        }
+        fclose(infile);
     }
     else{
-	fprintf(stderr, "Must specify input file path\n");
-	exit(EXIT_FAILURE);
+        fprintf(stderr, "Must specify input file path\n");
+        exit(EXIT_FAILURE);
     }
 
     fprintf(stdout, "Unsorted:\n");
     for(i = 0; i < len; i++){
-	fprintf(stdout, "%d ", values[i]);
+        fprintf(stdout, "%d ", values[i]);
     }
     fprintf(stdout, "\n");
 
@@ -110,7 +120,7 @@ int main(int argv, char* argc[]){
 
     fprintf(stdout, "Sorted:\n");
     for(i = 0; i < len; i++){
-	fprintf(stdout, "%d ", values[i]);
+        fprintf(stdout, "%d ", values[i]);
     }
     fprintf(stdout, "\n");
 
